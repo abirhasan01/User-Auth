3c7ba24d1ac591049bc3d275880a959bd33bee86
abirhasan01 <appleabirhasan@gmail.com>
2025-11-21 06:51:55 -0800

    add frontend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("./config/db")
 const express = require("express")
 const  userRoute  = require("./routes/user.route")
 const { notFound, errorHandler } = require("./middleware/error.middleware")
+const cors = require("cors")
 const port = process.env.PORT || 4000
 const app = express()
 const cookieParser = require("cookie-parser");
@@ -12,6 +13,7 @@ const cookieParser = require("cookie-parser");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("server is ready")
